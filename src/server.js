747fed58004bfdea02532bec09ada58353cb8e0c
mcurrr <technosigal@gmail.com>
2018-02-16 01:27:27 +0200

    add mongo to dev

--- src/server.js
+++ src/server.js
@@ -2,10 +2,28 @@
 const express = require('express');
 const lodash = require('lodash')
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+ 
 const { find, random } = lodash;
 
+mongoose.connect('mongodb://mongo:27017', function (err) {
+   if (err) throw err;
+   console.log('Successfully connected');
+});
+
+const Cat = mongoose.model('Cat', {
+    id: mongoose.Schema.Types.ObjectId,
+    name: String,
+});
+
+const kitty = new Cat({
+    name: 'Zildjian',
+});
+
+kitty.save().then(() => console.log('meow'));
+
 const app = express();
+
 
 const CATS = [
     {id: 101, name: 'Fluffy'},
@@ -15,11 +33,20 @@ const CATS = [
     {id: 105, name: 'Jessica'},
 ];
 
-
-app.use(cors());
+// app.use(cors());
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => res.send('Test API for mr.Eagle'));
+
+app.get('/new-cats/', (req, res) => {
+    Cat.find({}, function(err, cats) {
+        if (err) throw err;
+        // object of all the users
+        console.log(cats);
+        res.send(cats)
+    });
+});
+
 app.get('/cats/', (req, res) => res.send(CATS));
 
 app.get('/cat/:id/', function (req, res) {
