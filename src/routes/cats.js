c4f436c6f324c3c67431a7d2472d751551eaf825
mcurrr <technosigal@gmail.com>
2018-02-21 00:19:44 +0200

    first attempt to create search

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { every, isEmpty, merge, map } = require('lodash');
+const { every, isEmpty, merge, map, reduce, keys } = require('lodash');
 const colors = require('colors/safe');
 
 const { Cat } = require('../schema');
 const CAT_LIMIT = 10;
 
+function convertQueryValue(key, value) {
+    switch (key) {
+        case 'name': return new RegExp(value, 'i');
+        case 'description': return new RegExp(value, 'i');
+        // case 'age': return { $gte: value.from, $lte: value.to };
+        case 'likes': return { $in: value.split(',') };
+        default: return value;
+    }
+}
+
 // get all cats
 router.get('/all/', (req, res) => {
-    // const search = req.param('search');
-    // const skip = req.param('skip');
-    // console.log(colors.green(search, skip));
+    const query = reduce(req.query, (acc, value, key) => {
+        if (isEmpty(value)) return acc;
+        return { ...acc, [key]: convertQueryValue(key, value) };
+    }, {});
 
     Cat
-        .find({})
-        .sort({ created: -1 })
+        .find(query)
+        .sort({ created_at: -1 })
         // .limit(CAT_LIMIT)
-        .select('name')
+        .select(`name created_at ${keys(query).join(' ')}`)
         .exec((err, cats) => {
         if (err) res.status(500).json({ error: err.message }).end();
         res.json({ result: map(cats, cat => cat.toJSON()) });
@@ -43,16 +54,26 @@ router.get('/get/:id/', function (req, res) {
 });
 
 // change cat
-router.put('change/:id/', function (req, res) {
+router.put('change/:id/', (req, res) => {
     const id = req.params.id;
 
-    Cat.findByIdAndUpdate(id, req.body, function(err, cat) {
+    Cat.findByIdAndUpdate(id, req.body, (err, cat) => {
         if (err) throw err;
         res.status(200).json({ result: cat.toJSON() });
     });
 });
 
-router.get('*', function(req, res){
+// get cat
+router.post('/remove/:id/', (req, res) => {
+    const id = req.params.id;
+    Cat
+        .remove({ _id: id }, err => {
+            if (err) res.status(500).json({ error: err.message }).end();
+            res.json({ result: 'deleted' });
+        });
+});
+
+router.get('*', (req, res) => {
     res.send('invalid route', 404);
 });
 
